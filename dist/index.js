f169f13598bba23d166b5406147ab65b1f27de48
itl95pps <itl95pps@gmail.com>
2021-07-06 11:42:42 -0300

    teste soucelevel

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,11 @@
 console.log("meu projeto buildado :)");
 console.log("uh novo milestone");
+
+// isso vai dar erro de proposito
+const user = null;
+user.name = "User";
+
+// loop infinito
+for (let index = 0; index < -1; index++) {
+  console.log("infinito");
+}
